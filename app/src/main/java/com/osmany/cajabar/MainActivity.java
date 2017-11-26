8fe9f35424fb3876772296d59136d2f751d41a0e
Osmany <osmany.rodriguez101@gmail.com>
2017-11-26 16:00:43 +0000

    paso de gigante

--- app/src/main/java/com/osmany/cajabar/MainActivity.java
+++ app/src/main/java/com/osmany/cajabar/MainActivity.java
@@ -3,27 +3,29 @@ package com.osmany.cajabar;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Gravity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
-import android.widget.ScrollView;
 import android.widget.TextView;
 
 import java.util.Iterator;
 
 import static com.osmany.cajabar.ProductosActivity.CARGA_DATOS_CONFIGURACION;
+import static com.osmany.cajabar.ProductosActivity.LISTA_PRODUCTOS_NUEVOS;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     public static final String PEDIDO_NUEVO = "pedidoNuevo";
+    public static final int REQUEST_CODE = 10;
     Propiedades propiedades = new Propiedades();
     Button añadir;
     TextView pedido;
-    Stock stockNuevoProducto;
-    ScrollView miScroll;
+    Stock stockNuevoProducto = new Stock();
+    LinearLayout miLineal;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,8 +33,10 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         setContentView(R.layout.activity_main);
         añadir=findViewById(R.id.btn_añadir_main_activity);
         añadir.setOnClickListener(this);
-        stockNuevoProducto = (Stock) getIntent().getSerializableExtra(PEDIDO_NUEVO);
-        miScroll = findViewById(R.id.scroll_avtivity_main);
+
+        miLineal = findViewById(R.id.linea_del_Scroll);
+
+
 
     }
 
@@ -56,33 +60,57 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         switch (view.getId()) {
             case R.id.btn_añadir_main_activity:
                 Intent inte = new Intent(this, ProductosActivity.class);
+                inte.putExtra(LISTA_PRODUCTOS_NUEVOS, stockNuevoProducto);
                 inte.putExtra(CARGA_DATOS_CONFIGURACION, propiedades);
-                startActivity(inte);
+                startActivityForResult(inte, REQUEST_CODE);
                 break;
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == REQUEST_CODE && resultCode == RESULT_OK) {
+            stockNuevoProducto = (Stock) data.getSerializableExtra(PEDIDO_NUEVO);
+            creaLayouts();
         }
     }
 
     public void creaLayouts() {
         Iterator<Productos> iterator = stockNuevoProducto.getList().iterator();
+        LinearLayout linearLayout = new LinearLayout(this);
+        linearLayout.setGravity(Gravity.CENTER_VERTICAL);
+        LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, 200);
+
+        linearLayout.setLayoutParams(params);
+
         while (iterator.hasNext()) {
+            LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+            layoutParams.setMargins(100, 0, 0, 0);
             Productos producto = iterator.next();
-            LinearLayout linearLayout = new LinearLayout(this);
+
 
             ImageView imageView = new ImageView(this);
             imageView.setImageResource(producto.getFoto());
 
             TextView tvNombre = new TextView(this);
             tvNombre.setText(producto.getName());
+            tvNombre.setLayoutParams(layoutParams);
+
 
             TextView tvPrecio = new TextView(this);
+
+            tvPrecio.setLayoutParams(layoutParams);
             tvPrecio.setText(Double.toString(producto.getPrecio()) + propiedades.getMoneda());
+
 
 
             TextView tvCantidad = new TextView(this);
             tvCantidad.setText("1");
+            tvCantidad.setLayoutParams(layoutParams);
 
             TextView tvTotal = new TextView(this);
-
+            tvTotal.setLayoutParams(layoutParams);
             double total = producto.getPrecio() * Integer.parseInt(tvCantidad.getText().toString());
 
             tvTotal.setText(Double.toString(total));
@@ -93,8 +121,10 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             linearLayout.addView(tvPrecio);
             linearLayout.addView(tvCantidad);
             linearLayout.addView(tvTotal);
-
+            miLineal.addView(linearLayout);
 
         }
+
+
     }
 }
--- app/src/main/java/com/osmany/cajabar/ProductosActivity.java
+++ app/src/main/java/com/osmany/cajabar/ProductosActivity.java
@@ -1,11 +1,14 @@
 package com.osmany.cajabar;
 
 import android.content.Intent;
+import android.graphics.Point;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Display;
 import android.view.Gravity;
 import android.view.View;
-import android.widget.ImageButton;
+import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TableLayout;
 import android.widget.TableRow;
 import android.widget.TextView;
@@ -18,6 +21,7 @@ import static com.osmany.cajabar.MainActivity.PEDIDO_NUEVO;
 
 public class ProductosActivity extends AppCompatActivity implements View.OnClickListener {
     public static final String CARGA_DATOS_CONFIGURACION = "./ProdcuctosActivity";
+    public static final String LISTA_PRODUCTOS_NUEVOS = "listaProductos";
     Propiedades propiedades;
     Stock miStock = new Stock();
     Stock listaPedido;
@@ -26,12 +30,14 @@ public class ProductosActivity extends AppCompatActivity implements View.OnClick
     Iterator<Productos> iterator = miStock.getList().iterator();
     TableRow filaButton;
     TableRow filaPrecio;
-    ImageButton imageButton;
+    ImageView imageView;
     TextView textPrecio;
     String cadenatMoneda;
     TextView cantidad_productos;
     TextView precio_producto;
     TextView moneda;
+    Button añadirProducto, sumarProducto, restarProducto, cancelarProducto;
+    double primerPrecio = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,16 +48,30 @@ public class ProductosActivity extends AppCompatActivity implements View.OnClick
         setContentView(R.layout.activity_productos);
         miTable = findViewById(R.id.table_layout);
         propiedades = (Propiedades) getIntent().getSerializableExtra(CARGA_DATOS_CONFIGURACION);
+        listaPedido = (Stock) getIntent().getSerializableExtra(LISTA_PRODUCTOS_NUEVOS);
         cadenatMoneda = (propiedades.getMoneda());
         crearLayoutProductos();
         cantidad_productos = findViewById(R.id.tv_cantidad_productos);
         precio_producto = findViewById(R.id.tv_precio_producto);
         moneda = findViewById(R.id.tv_moneda);
-
+        añadirProducto = findViewById(R.id.btn_añadir_productos_activity);
+        añadirProducto.setOnClickListener(this);
+        sumarProducto = findViewById(R.id.btn_Suma);
+        sumarProducto.setOnClickListener(this);
+        restarProducto = findViewById(R.id.btn_Resta);
+        restarProducto.setOnClickListener(this);
+        cancelarProducto = findViewById(R.id.btn_cancelar_productos_activity);
+        cancelarProducto.setOnClickListener(this);
     }
 
 
     public void crearLayoutProductos() {
+        Display ventana = getWindowManager().getDefaultDisplay();
+        Point tamaño = new Point();
+        ventana.getSize(tamaño);
+        int ancho = tamaño.x;
+        int tamañoTotal = ancho / 3;
+
         int contadorFila = 0;
         while (iterator.hasNext()) {
 
@@ -61,13 +81,18 @@ public class ProductosActivity extends AppCompatActivity implements View.OnClick
             filaButton.setGravity(Gravity.CENTER);
             while (contadorFila != 3) {
                 Productos pr = iterator.next();
+                TableRow.LayoutParams params = new TableRow.LayoutParams();
+                params.width = tamañoTotal;
+                params.height = tamañoTotal;
 
-                imageButton = new ImageButton(this);
-                imageButton.setImageResource(pr.getFoto());
-                imageButton.setClickable(true);
-                imageButton.setId(pr.getId());
-                imageButton.setOnClickListener(this);
-                filaButton.addView(imageButton);
+                imageView = new ImageView(this);
+                imageView.setScaleType(ImageView.ScaleType.FIT_CENTER);
+                imageView.setLayoutParams(params);
+                imageView.setImageResource(pr.getFoto());
+                imageView.setClickable(true);
+                imageView.setId(pr.getId());
+                imageView.setOnClickListener(this);
+                filaButton.addView(imageView);
 
                 textPrecio = new TextView(this);
                 textPrecio.setGravity(Gravity.CENTER);
@@ -94,109 +119,177 @@ public class ProductosActivity extends AppCompatActivity implements View.OnClick
             case 1:
                 setTitle(miStock.getList().get(0).getName());
                 precio_producto.setText(Double.toString(miStock.getList().get(0).getPrecio()));
+                cantidad_productos.setText("1");
+                primerPrecio = miStock.getList().get(0).getPrecio();
                 break;
             case 2:
                 setTitle(miStock.getList().get(1).getName());
                 precio_producto.setText(Double.toString(miStock.getList().get(1).getPrecio()));
+                primerPrecio = miStock.getList().get(1).getPrecio();
+                cantidad_productos.setText("1");
                 break;
             case 3:
                 setTitle(miStock.getList().get(2).getName());
                 precio_producto.setText(Double.toString(miStock.getList().get(2).getPrecio()));
+                primerPrecio = miStock.getList().get(2).getPrecio();
+                cantidad_productos.setText("1");
                 break;
             case 4:
                 setTitle(miStock.getList().get(3).getName());
                 precio_producto.setText(Double.toString(miStock.getList().get(3).getPrecio()));
+                primerPrecio = miStock.getList().get(3).getPrecio();
+                cantidad_productos.setText("1");
                 break;
             case 5:
                 setTitle(miStock.getList().get(4).getName());
                 precio_producto.setText(Double.toString(miStock.getList().get(4).getPrecio()));
+                primerPrecio = miStock.getList().get(4).getPrecio();
+                cantidad_productos.setText("1");
                 break;
             case 6:
                 setTitle(miStock.getList().get(5).getName());
                 precio_producto.setText(Double.toString(miStock.getList().get(5).getPrecio()));
+                primerPrecio = miStock.getList().get(5).getPrecio();
+                cantidad_productos.setText("1");
                 break;
             case 7:
                 setTitle(miStock.getList().get(6).getName());
                 precio_producto.setText(Double.toString(miStock.getList().get(6).getPrecio()));
+                primerPrecio = miStock.getList().get(6).getPrecio();
+                cantidad_productos.setText("1");
                 break;
             case 8:
                 setTitle(miStock.getList().get(7).getName());
                 precio_producto.setText(Double.toString(miStock.getList().get(7).getPrecio()));
+                primerPrecio = miStock.getList().get(9).getPrecio();
+                cantidad_productos.setText("1");
                 break;
             case 9:
                 setTitle(miStock.getList().get(8).getName());
                 precio_producto.setText(Double.toString(miStock.getList().get(8).getPrecio()));
+                primerPrecio = miStock.getList().get(8).getPrecio();
+                cantidad_productos.setText("1");
                 break;
             case 10:
                 setTitle(miStock.getList().get(9).getName());
                 precio_producto.setText(Double.toString(miStock.getList().get(9).getPrecio()));
+                primerPrecio = miStock.getList().get(9).getPrecio();
+                cantidad_productos.setText("1");
                 break;
             case 11:
                 setTitle(miStock.getList().get(10).getName());
                 precio_producto.setText(Double.toString(miStock.getList().get(10).getPrecio()));
+                primerPrecio = miStock.getList().get(10).getPrecio();
+                cantidad_productos.setText("1");
                 break;
             case 12:
                 setTitle(miStock.getList().get(11).getName());
                 precio_producto.setText(Double.toString(miStock.getList().get(11).getPrecio()));
+                primerPrecio = miStock.getList().get(11).getPrecio();
+                cantidad_productos.setText("1");
                 break;
             case 13:
                 setTitle(miStock.getList().get(12).getName());
                 precio_producto.setText(Double.toString(miStock.getList().get(12).getPrecio()));
+                primerPrecio = miStock.getList().get(12).getPrecio();
+                cantidad_productos.setText("1");
+
                 break;
             case 14:
                 setTitle(miStock.getList().get(13).getName());
                 precio_producto.setText(Double.toString(miStock.getList().get(13).getPrecio()));
+                primerPrecio = miStock.getList().get(13).getPrecio();
+                cantidad_productos.setText("1");
                 break;
             case 15:
                 setTitle(miStock.getList().get(14).getName());
                 precio_producto.setText(Double.toString(miStock.getList().get(14).getPrecio()));
+                primerPrecio = miStock.getList().get(14).getPrecio();
+                cantidad_productos.setText("1");
                 break;
             case 16:
                 setTitle(miStock.getList().get(15).getName());
                 precio_producto.setText(Double.toString(miStock.getList().get(15).getPrecio()));
+                primerPrecio = miStock.getList().get(15).getPrecio();
+                cantidad_productos.setText("1");
                 break;
             case 17:
                 setTitle(miStock.getList().get(16).getName());
                 precio_producto.setText(Double.toString(miStock.getList().get(16).getPrecio()));
+                primerPrecio = miStock.getList().get(16).getPrecio();
+                cantidad_productos.setText("1");
                 break;
             case 18:
                 setTitle(miStock.getList().get(17).getName());
                 precio_producto.setText(Double.toString(miStock.getList().get(17).getPrecio()));
+                primerPrecio = miStock.getList().get(17).getPrecio();
+                cantidad_productos.setText("1");
                 break;
             case 19:
                 setTitle(miStock.getList().get(18).getName());
                 precio_producto.setText(Double.toString(miStock.getList().get(18).getPrecio()));
+                primerPrecio = miStock.getList().get(18).getPrecio();
+                cantidad_productos.setText("1");
                 break;
             case 20:
                 setTitle(miStock.getList().get(19).getName());
                 precio_producto.setText(Double.toString(miStock.getList().get(19).getPrecio()));
+                primerPrecio = miStock.getList().get(19).getPrecio();
+                cantidad_productos.setText("1");
                 break;
             case 21:
                 setTitle(miStock.getList().get(20).getName());
                 precio_producto.setText(Double.toString(miStock.getList().get(20).getPrecio()));
+                primerPrecio = miStock.getList().get(20).getPrecio();
+                cantidad_productos.setText("1");
+                break;
+            case R.id.btn_añadir_productos_activity:
+                String nom = getTitle().toString();
+                int cant = Integer.parseInt(cantidad_productos.getText().toString());
+
+                Iterator<Productos> iterator = miStock.getList().iterator();
+
+                while (iterator.hasNext()) {
+                    Productos producto = iterator.next();
+                    if (producto.getName().equals(nom)) {
+
+                        pedidoNuevo.add(producto);
+
+                    }
+
+                }
+                listaPedido.setList(pedidoNuevo);
+                Intent intent = new Intent(this, MainActivity.class);
+                intent.putExtra(PEDIDO_NUEVO, listaPedido);
+                intent.putExtra("cantidad", cant);
+                setResult(RESULT_OK, intent);
+                finish();
+                break;
+
+            case R.id.btn_Suma:
+                if (Integer.parseInt(cantidad_productos.getText().toString()) != 10 && !getTitle().equals("CajaBar")) {
+
+                    int sumaCantidad = Integer.parseInt(cantidad_productos.getText().toString()) + 1;
+                    double sumaPrecio = primerPrecio * sumaCantidad;
+                    cantidad_productos.setText(Integer.toString(sumaCantidad));
+                    precio_producto.setText(Double.toString(sumaPrecio));
+                }
+                break;
+            case R.id.btn_Resta:
+                if (Integer.parseInt(cantidad_productos.getText().toString()) != 1 && !getTitle().equals("CajaBar")) {
+
+                    int restaCantidad = Integer.parseInt(cantidad_productos.getText().toString()) - 1;
+                    cantidad_productos.setText(Integer.toString(restaCantidad));
+                    double restaPrecio = Double.parseDouble(precio_producto.getText().toString()) - primerPrecio;
+                    precio_producto.setText(Double.toString(restaPrecio));
+                }
+                break;
+            case R.id.btn_cancelar_productos_activity:
+                finish();
                 break;
         }
 
     }
 
-    public void eventoAñadir(View view) {
 
-        String nom = getTitle().toString();
-        int cant = Integer.parseInt(cantidad_productos.getText().toString());
-        Iterator<Productos> iterator = miStock.getList().iterator();
-        while (iterator.hasNext()) {
-            Productos producto = iterator.next();
-            if (producto.getName().equals(nom)) {
-                for (int i = 1; i <= cant; i++) {
-                    pedidoNuevo.add(producto);
-                }
-            }
-        }
-        listaPedido.setList(pedidoNuevo);
-        Intent intent = new Intent(this, MainActivity.class);
-        intent.putExtra(PEDIDO_NUEVO, listaPedido);
-        startActivity(intent);
-        this.finish();
-
-    }
 }
